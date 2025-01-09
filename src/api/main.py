17cf408bf59eb11333e89fbf904c1730938873d0
Vinay <brvkr5794@gmail.com>
2025-01-08 20:17:17 -0600

    remove trailing slash CORS

--- src/api/main.py
+++ src/api/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-    "https://iso-assist.onrender.com/",
+    "https://iso-assist.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
